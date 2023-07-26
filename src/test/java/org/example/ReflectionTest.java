96a3b68808da9ce68190edadea951de3af745bbb
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-07-26 16:30:08 +0900

    í힙영역에서 클래스 가져오기

--- src/test/java/org/example/ReflectionTest.java
+++ src/test/java/org/example/ReflectionTest.java
@@ -15,6 +15,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @Controller 애노테이션이 설정돼 있는 모든 클래스를 찾아서 출력한다.
  */
@@ -42,6 +44,30 @@ public class ReflectionTest {
         // [public boolean org.example.model.User.equals(java.lang.Object), public int org.example.model.User.hashCode(), public boolean org.example.model.User.equalUser(org.example.model.User)]
     }
 
+    /**
+     * 힙 영역에 로드돼 있는 클래스 타입의 객체를 통해 필드/메소드/생성자를 접근 제어자와 상관 없이 사용할 수 있도록 지원하는 API
+     */
+    @Test
+    void load() throws ClassNotFoundException {
+        // 1
+        Class<User> clazz = User.class;
+
+        // 2
+        User user = new User("serverwizard", "홍종완");
+        Class<? extends User> clazz2 = user.getClass();
+
+        // 3
+        Class<?> clazz3 = Class.forName("org.example.model.User");
+
+        logger.debug("clazz: [{}]", clazz);
+        logger.debug("clazz2: [{}]", clazz2);
+        logger.debug("clazz3: [{}]", clazz3);
+
+        assertThat(clazz == clazz2).isTrue();
+        assertThat(clazz2 == clazz3).isTrue();
+        assertThat(clazz3 == clazz).isTrue();
+
+    }
 
     private static Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
         Reflections reflections =  new Reflections("org.example"); // org.example 아래 모든 파일에 대해서 reflection 사용 의미
