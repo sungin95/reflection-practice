a46c56548b6abd101872348708817910daa02480
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-07-26 16:26:49 +0900

    showClass

--- src/test/java/org/example/ReflectionTest.java
+++ src/test/java/org/example/ReflectionTest.java
@@ -2,15 +2,18 @@ package org.example;
 
 import org.example.annotation.Controller;
 import org.example.annotation.Service;
+import org.example.model.User;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @Controller 애노테이션이 설정돼 있는 모든 클래스를 찾아서 출력한다.
@@ -24,10 +27,27 @@ public class ReflectionTest {
         logger.debug("beans: [{}]", beans);
     }
 
+    @Test
+    void showClass() {
+        Class<User> clazz = User.class;
+        logger.debug(clazz.getName());
+
+        logger.debug("User all declared fields: [{}]", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
+        // [private java.lang.String org.example.model.User.userId, private java.lang.String org.example.model.User.name]
+
+        logger.debug("User all declared constructors: [{}]", Arrays.stream(clazz.getDeclaredConstructors()).collect(Collectors.toList()));
+        // [public org.example.model.User(java.lang.String,java.lang.String)]
+
+        logger.debug("User all declared methods: [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
+        // [public boolean org.example.model.User.equals(java.lang.Object), public int org.example.model.User.hashCode(), public boolean org.example.model.User.equalUser(org.example.model.User)]
+    }
+
+
     private static Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
         Reflections reflections =  new Reflections("org.example"); // org.example 아래 모든 파일에 대해서 reflection 사용 의미
 
         Set<Class<?>> beans = new HashSet<>();
+//        System.out.println(annotations); => [interface org.example.annotation.Controller, interface org.example.annotation.Service]
         annotations.forEach(annotation -> beans.addAll(reflections.getTypesAnnotatedWith(annotation)));
 
         return beans;
